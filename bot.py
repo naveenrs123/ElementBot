13d84855f414e9f52e5ba8b684792e3bbe3ed7d5
naveenrs123 <naveen.sivasankar1234@gmail.com>
2020-12-13 23:41:59 -0800

    Initial commit

---
+++ bot.py
@@ -0,0 +1,33 @@
+# bot.py
+import os
+import random
+from dotenv import load_dotenv
+
+# 1
+import discord
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+
+# 2
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    members = '\n - '.join([member.name for member in guild.members])
+    print(members)
+
+@bot.command(name="hi", help="Responds with a fingerguns emoji.")
+async def fingerguns_hello(ctx):
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    fingerguns = discord.utils.get(guild.emojis, name="fingerguns")
+    await ctx.send(f"<:{fingerguns.name}:{fingerguns.id}>")
+
+bot.run(TOKEN)
