95c44b708da40c7878ddf61c99be0d6289d9de3a
naveenrs123 <naveen.sivasankar1234@gmail.com>
2020-12-14 21:14:39 -0800

    Add motivational quotes command

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-# from dotenv import load_dotenv - FOR LOCAL USE
+#from dotenv import load_dotenv # FOR LOCAL USE
 import os
+import requests
 import discord
 from discord.ext import commands
 
-# load_dotenv() - FOR LOCAL USE
+#load_dotenv() # FOR LOCAL USE
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
@@ -15,10 +16,31 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@bot.command(name="hi", help="Responds with a fingerguns emoji.")
+@bot.command(name="hi", help=f"Responds with the fingerguns emoji.")
 async def fingerguns_hello(ctx):
     guild = discord.utils.get(bot.guilds, name=GUILD)
     fingerguns = discord.utils.get(guild.emojis, name="fingerguns")
     await ctx.send(f"<:{fingerguns.name}:{fingerguns.id}>")
+
+@bot.command(name="motivate", help="Responds with a motivational quote.")
+async def motivate(ctx):
+    message = ""
+    response = requests.get("https://zenquotes.io/api/random")
+    if response.status_code == 200:
+        quote = response.json()
+        message =quote[0]['q']
+        author = quote[0]['a']
+        if message.startswith("Too many requests."):
+            await ctx.send("The Zenquotes API is overloaded. Please wait 30 seconds before trying again.")
+        else:
+            embedQuote = discord.Embed(title="Motivational Quotes", type="rich", url="https://zenquotes.io/")
+            embedQuote.color = discord.Color.from_rgb(52, 29, 105)
+            embedQuote.set_thumbnail(url="https://user-images.githubusercontent.com/42954045/102170926-49b47f80-3e4a-11eb-929b-eed606a70399.png") \
+                      .add_field(name='Quote:',value="_" + message + "_", inline=False) \
+                      .add_field(name='Author:', value=author, inline=False) \
+                      .set_footer(text="Quotes provided by ZenQuotes", icon_url=bot.user.avatar_url)
+            await ctx.send(embed=embedQuote)
+    else:
+        await ctx.send("An unknown error occurred. Please try again later.")
 
 bot.run(TOKEN)
